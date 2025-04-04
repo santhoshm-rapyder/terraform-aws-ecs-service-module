0ca68af52530966d34f91825365b07dd7f21aced
santhoshm-rapyder <santhosh.m@rapyder.com>
2025-04-04 15:02:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,41 @@
 resource "aws_ecs_service" "ecs_service" {
   name            = "${var.environment}-ecs-service"
-  cluster        = var.cluster_id
+  cluster         = var.cluster_id
   task_definition = var.task_definition_arn
   desired_count   = var.desired_count
   launch_type     = "EC2"
 
-  network_configuration {
-    subnets          = var.private_subnet_ids
-    security_groups  = [var.security_group_id]
+  # Network configuration is only applied when using 'awsvpc' mode
+  dynamic "network_configuration" {
+    for_each = var.network_mode == "awsvpc" ? [1] : []
+    content {
+      subnets         = var.private_subnet_ids
+      security_groups = [var.security_group_id]
+    }
   }
 
+  # Load balancer settings
   load_balancer {
     target_group_arn = var.target_group_arn
     container_name   = var.container_name
     container_port   = var.container_port
   }
 
-  service_connect_configuration {
-    enabled = true
-    namespace = var.service_connect_namespace
+  # Service Connect Configuration (Only for awsvpc mode)
+  dynamic "service_connect_configuration" {
+    for_each = var.enable_service_connect && var.network_mode == "awsvpc" ? [1] : []
+    content {
+      enabled   = true
+      namespace = var.service_connect_namespace
 
-    service {
-      discovery_name  = var.container_name
-      client_alias {
-        dns_name = var.container_name
-        port     = var.container_port
+      service {
+        discovery_name = var.container_name
+        client_alias {
+          dns_name = var.container_name
+          port     = var.container_port
+        }
+        port_name = "service-connect"
       }
-      port_name = "service-connect"
     }
   }
 
@@ -34,7 +43,6 @@ resource "aws_ecs_service" "ecs_service" {
     ignore_changes = [task_definition]
   }
 }
-
 
 # Auto Scaling Target
 resource "aws_appautoscaling_target" "ecs_target" {
